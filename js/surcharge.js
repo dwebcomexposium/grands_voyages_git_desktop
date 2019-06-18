52aed9b44478ee24f4d89af6641766caa1afde79
CharlieSaso <charlie.sasorith@gmail.com>
2019-06-18 16:24:14 +0200

    Update surcharge.js

--- js/surcharge.js
+++ js/surcharge.js
@@ -1,11 +1,3 @@
-/*!
- * charlie
- * 
- * 
- * @author charlie
- * @version 1.0.0
- * Copyright 2019. ISC licensed.
- */
 'use strict';
 
 
@@ -13,23 +5,14 @@ $(document).ready(function(){
 
 /*HOME*/
 	/*MAP*/
-        $('li.state').click(function(){
-                console.log("éeae");
-        });
-
-	$('li.state').hover(function(){
-                console.log("ese");
-
-                $('li.state').each(function(){
-                        $(this).removeClass('active');
-                });
+	$('.custom_fk ul li').hover(function(){
+                $('.custom_fk ul li.active').removeClass('active');
                 $('.map img').removeClass('active');
-                $('.custom_fk p.state').removeClass('active');
+                $('.custom_fk .state').removeClass('active');
                 var pays = $( this ).attr('data-item');
-                console.log(pays);
                 $(this).addClass('active');
                 $('.map .'+pays+'').addClass('active');
-                $('.custom_fk p.'+pays+'').addClass('active');
+                $('.state.'+pays+'').addClass('active');
 	});
 
 });
